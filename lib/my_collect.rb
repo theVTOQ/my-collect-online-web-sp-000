af1d3cdb60d62b5935251c00b163c330a5621d97
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-11 17:16:04 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(collection)
   i = 0
   new_collection = []
   while i < collection.size
-    i += 1
     new_collection << yield(collection[i])
+    i += 1
   end
   new_collection
 end
